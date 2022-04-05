26bc58e134249f3ee84f0c97dfb54cb6c8d82344
Rami <ramiakl33@gmail.com>
2022-04-05 18:51:18 +0300

    Finishing the api

--- BackEnd/insert_to_pantry_api.php
+++ BackEnd/insert_to_pantry_api.php
@@ -9,6 +9,13 @@ $location = $_POST["location"];
 $weight = $_POST["weight"];
 
 
+$json_response = json_encode($response);
+echo($json_response);
 
+// i will later hash the password 
+
+$query = $mysqli->prepare("INSERT INTO pantry (item_name, date_of_expiration, location, Weight) VALUES (?, ?, ?,?)");
+$query->bind_param("ssss", $item, $expiration_date, $location,$weight);
+$query->execute();
 
 ?>
