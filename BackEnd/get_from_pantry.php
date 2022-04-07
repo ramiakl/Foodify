0f96956b5ff57777c1d838a277f3d25f0d3994e0
Rami <ramiakl33@gmail.com>
2022-04-07 16:20:19 +0300

    Creating an API that get the items from the pantry

---
+++ BackEnd/get_from_pantry.php
@@ -0,0 +1,19 @@
+<?php
+
+include("db_info.php");
+
+$query = $mysqli->prepare("SELECT * FROM pantry;");
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+
+while($course = $array->fetch_assoc()){
+    $response[] = $course;
+}
+
+$json_response = json_encode($response);
+echo $json_response;
+
+?>
--- BackEnd/insert_to_pantry_api.php
+++ BackEnd/insert_to_pantry_api.php
@@ -12,8 +12,6 @@ $weight = $_POST["weight"];
 $json_response = json_encode($response);
 echo($json_response);
 
-// i will later hash the password 
-
 $query = $mysqli->prepare("INSERT INTO pantry (item_name, date_of_expiration, location, Weight) VALUES (?, ?, ?,?)");
 $query->bind_param("ssss", $item, $expiration_date, $location,$weight);
 $query->execute();
