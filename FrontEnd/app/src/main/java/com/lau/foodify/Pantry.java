7d00ff3a182b4c9a3d907d8f7c4d19d06bc898ec
Rami <ramiakl33@gmail.com>
2022-04-11 20:08:21 +0300

    Creating a list of food and an adapter

--- FrontEnd/app/src/main/java/com/lau/foodify/Pantry.java
+++ FrontEnd/app/src/main/java/com/lau/foodify/Pantry.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView my_list;
+    ArrayList<String> the_list;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
