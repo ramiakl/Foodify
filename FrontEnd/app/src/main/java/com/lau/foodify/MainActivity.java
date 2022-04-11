677fd840ca00c1a26ddc7eedd07ba3270d5eff4f
Rami <ramiakl33@gmail.com>
2022-04-11 23:21:43 +0300

    Adding a bar at the bottom of the UI

--- FrontEnd/app/src/main/java/com/lau/foodify/MainActivity.java
+++ FrontEnd/app/src/main/java/com/lau/foodify/MainActivity.java
@@ -3,12 +3,30 @@ package com.lau.foodify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.list);
+
+
+        items = new ArrayList<String>(Arrays.asList("Mobile Computing", "Database Management Systems", "Software Engineering"));
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        list.setAdapter(adapter);
+
     }
 }
