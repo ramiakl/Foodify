f57c601aca497fe5fb13790c6e80cf71f7dd7ab2
Rami <ramiakl33@gmail.com>
2022-04-12 00:11:13 +0300

    Connecting the signup to a class

--- FrontEnd/app/src/main/java/com/lau/foodify/Login.java
+++ FrontEnd/app/src/main/java/com/lau/foodify/Login.java
@@ -2,13 +2,28 @@ package com.lau.foodify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.login);
+
+
+    }
+
+    public void login (View view){
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void signup(View view){
+        Intent intent = new Intent(getApplicationContext(), Signup.class);
+        startActivity(intent);
     }
 }
