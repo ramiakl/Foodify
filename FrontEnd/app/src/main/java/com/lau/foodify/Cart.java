f4025312c54bf7a0430c5d294907281625e627db
Rami <ramiakl33@gmail.com>
2022-04-12 13:55:08 +0300

    Creating a cookbook UI

--- FrontEnd/app/src/main/java/com/lau/foodify/Cart.java
+++ FrontEnd/app/src/main/java/com/lau/foodify/Cart.java
@@ -3,12 +3,19 @@ package com.lau.foodify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Cart extends AppCompatActivity {
+
+    double price;
+    TextView total_price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cart);
+
+        total_price = (TextView) findViewById(R.id.price);
+
     }
 }
