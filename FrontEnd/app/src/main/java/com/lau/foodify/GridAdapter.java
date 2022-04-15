87bd2f2ed88a2212ccd82248454ab285febb87a5
Rami <ramiakl33@gmail.com>
2022-04-15 14:38:29 +0300

    Experimenting with the grid view

--- FrontEnd/app/src/main/java/com/lau/foodify/GridAdapter.java
+++ FrontEnd/app/src/main/java/com/lau/foodify/GridAdapter.java
@@ -1,2 +1,59 @@
-package com.lau.foodify;public class GridAdapter {
+package com.lau.foodify;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class GridAdapter extends BaseAdapter {
+    Context context;
+    String[] recipe;
+    int[] image;
+
+    LayoutInflater inflater;
+
+    public GridAdapter(Context context, String[] recipe, int[] image) {
+        this.context = context;
+        this.recipe = recipe;
+        this.image = image;
+    }
+
+    @Override
+    public int getCount() {
+        return recipe.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (view == null){
+
+            view = inflater.inflate(R.layout.grid_item,null);
+
+        }
+
+        ImageView imageView = view.findViewById(R.id.grid_image);
+        TextView textView = view.findViewById(R.id.item_name);
+
+        imageView.setImageResource(image[i]);
+        textView.setText(recipe[i]);
+
+        return null;
+    }
 }
